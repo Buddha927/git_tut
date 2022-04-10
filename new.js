7f051cafb2f36100701b18e9bd87eb374aeff9c3
Buddha <buddhakotireddy@gmail.com>
2022-04-09 23:57:30 -0500

    new feature

---
+++ new.js
@@ -0,0 +1,21 @@
+// from es 6 - let, const, var
+// basic tutorial
+
+// primitive types
+// variable declarations - let, var, const
+// non-primitive / reference types - arrays, objects
+const value = 0;
+console.log(returnWho, "def");
+function returnWho(){
+    var who = "BUddha"
+    // let name = "Gopi"
+    if(who === "BUddha") {
+        return "This is me";
+    }
+    else{
+        const name = "random user"
+    }
+    console.log(name);
+    return `This is ${who}`;
+}
+returnWho();
